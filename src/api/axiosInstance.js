c2914c826ac6a8c276e98929048eeb9431862708
Sumu749 <sumaiya.cse.tec@gmail.com>
2026-05-18 14:50:52 +0600

    admin sdk is configured from fontend

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,31 @@
 import axios from "axios";
+import app from "../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
+
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        const auth = getAuth(app);
+        const currentUser = auth.currentUser;
+
+        if (currentUser) {
+            const token = await currentUser.getIdToken();
+
+            if (config.headers) {
+                config.headers.Authorization = `Bearer ${token}`;
+            } else {
+                config.headers = {
+                    Authorization: `Bearer ${token}`,
+                };
+            }
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error),
+);
 
 export default axiosInstance;
